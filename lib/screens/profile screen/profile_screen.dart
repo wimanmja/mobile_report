8e37e879f895c7306adfb7ce1b2e8235ee787158
unknown <wimanmahroja@gmail.com>
2024-12-02 11:58:01 +0700

    Kerangka Screen Profil Done

--- lib/screens/profile screen/profile_screen.dart
+++ lib/screens/profile screen/profile_screen.dart
@@ -18,6 +18,145 @@ class _ProfileScreenState extends State<ProfileScreen> {
         centerTitle: true,
         titleTextStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 30, bottom: 30),
+              height: 170,
+              width: 170,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/contoh.jpg"),
+                  fit: BoxFit.cover,
+                )
+              ),
+            ),
+          ),
+          // Row Nama
+          Row(
+            children: [
+              Container(
+                height: 40,
+                width: 70,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Nama',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    )
+                  )
+                )
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width-102,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                padding: EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(14)
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Get name from API',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  )
+                )
+              )
+            ],
+          ),
+          // Row Email
+          Row(
+            children: [
+              Container(
+                height: 40,
+                width: 70,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Email',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    )
+                  )
+                )
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width-102,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                padding: EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(14)
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Get email from API',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    )
+                  )
+                )
+              )
+            ],
+          ),
+          // Row Divisi
+          Row(
+            children: [
+              Container(
+                height: 40,
+                width: 70,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Divisi',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    )
+                  )
+                )
+              ),
+              Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width-102,
+                margin: EdgeInsets.only(bottom: 4, left: 8, right: 8),
+                padding: EdgeInsets.only(left: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(14)
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Get division from API',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    )
+                  )
+                )
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
